f7fd6b15553affed89a6091b27fd6a11f7bf711d
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-06 12:59:27 +1200

    Added first 4 functions (WIP)

--- interface.py
+++ interface.py
@@ -1,22 +1,111 @@
-'''Rory Collins - Python interface to interact with SQL database - 29/04/2024'''
+'''Rory Collins - Python interface to interact with SQL database'''
 # imports#
 import sqlite3
-DATABASE = ".db"
-
+DATABASE = "brawlhalla.db"
 
 # functions#
-def print_all_():
+
+
+# func 1#
+def print_all_legends():
     # connect to database#
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
     # sql command query to execute#
-    sql = "SELECT"
+    sql = "SELECT LegendName FROM Legend;"
     cursor.execute(sql)
     results = cursor.fetchall()
     # print results#
-    print(results)
+    print('''Legends
+    ''')
+    for item in results:
+        print(f"{item[0]:<10}")
     # close db#
-    db.close
+    db.close()
+
+
+# func 2#
+def print_all_weapons():
+    # connect to database#
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # sql command query to execute#
+    sql = "SELECT WeaponName FROM Weapon;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print results#
+    print('''Weapons
+    ''')
+    for item in results:
+        print(f"{item[0]:<10}")
+    # close db#
+    db.close()
+
+
+# func 3#
+def print_all_legends_weapons():
+    # connect to database#
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # sql command query to execute#
+    sql = "SELECT LegendName,WeaponName FROM Legend,Weapon;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print results#
+    print('''Legends  Weapons
+    ''')
+    for item in results:
+        print(f"{item[0]:<10} {item[1]:<10}")
+    # close db#
+    db.close()
+
+
+# func 4#
+def print_all_legends_and_their_weapons():
+    # connect to database#
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    # sql command query to execute#
+    sql = "SELECT LegendName,Weapon1,Weapon2 FROM Legend;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    # print results#
+    print('''Legends    Weapon 1   Weapon 2
+    ''')
+    for item in results:
+        print(f"{item[0]:<10} {item[1]:<10} {item[2]:<10}")
+    # close db#
+    db.close()
+
 
 # main code#
-print_all()
+
+# ask what function users want to use#
+while True:
+    print('''
+Functions:
+1. Print all Legends
+2. Print all Weapons
+3. Print all Legends and Weapons
+4. Print all Legends and their Weapons
+5. Search for Legends with 1 Weapon
+6. Search for Legends with 2 Weapons
+''')
+
+    # account for invalid inputs#
+    try:
+        AskQuestion = int(input("What do you want to do? "))
+    except ValueError:
+        print("Not a valid function. Try again.")
+
+    # if users input is valid
+    if AskQuestion == 1:
+        print_all_legends()
+        break
+    if AskQuestion == 2:
+        print_all_weapons()
+        break
+
+    if AskQuestion == 4:
+        print_all_legends_and_their_weapons()
+        break
